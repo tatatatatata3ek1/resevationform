909cdba6f8778fbeef1bec058031f11e9a46b4f2
tatatatatata3ek1 <87006557+tatatatatata3ek1@users.noreply.github.com>
2022-01-10 21:10:21 +0900

    master

--- static/index.js
+++ static/index.js
@@ -20,14 +20,16 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var number = $('input[name="number"]').val();
+        var number = $('input[name="volume"]').val();
+        var name = $('input[name="name"]').val();
+        var adress = $('input[name="adress"]').val();
         var names = '';
         $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
 
-        var msg = `①氏名：${name} ②住所：${adress} ③希望日：${date} ④数量：${volume}`;
+        var msg = `①氏名:${name} ②住所:${adress} ③希望日:${date} ④数量:${volume}`;
         sendMessage(msg);
 
         return false;
