7e9252c434837a3cd1d284460c0bf235760e015b
tatatatatata3ek1 <87006557+tatatatatata3ek1@users.noreply.github.com>
2023-09-05 14:30:53 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,21 +1,3 @@
-$(function () {
-    // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
-        }
-    });
 
     // 送信
     $('form').submit(function () {
@@ -23,9 +5,6 @@ $(function () {
         var number = $('input[name="volume"]').val();
         var name = $('input[name="name"]').val();
         var adress = $('input[name="adress"]').val();
-        var names = '';
-        $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
 
